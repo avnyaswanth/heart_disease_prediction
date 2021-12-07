133a17db7b4e355ab596261559c1d564589492d1
avnyaswanth <yaswanthalapati20@gmail.com>
2021-12-07 12:12:58 +0530

    template changes

--- app.py
+++ app.py
@@ -10,20 +10,14 @@ app = Flask(__name__)
 def home():
     return render_template('heart_disease.html')
 
-# Bind predict function to URL
 @app.route('/predict', methods =['POST'])
 def predict():
     
-    # Put all form entries values in a list 
     features = [float(i) for i in request.form.values()]
-    # Convert features to array
     array_features = [np.array(features)]
-    # Predict features
     prediction = model.predict(array_features)
-    
     output = prediction
-    
-    # Check the output values and retrive the result with html tag based on the value
+
     if output == 1:
         return render_template('heart_disease.html', 
                                result = 'The patient is not likely to have heart disease!')
@@ -32,6 +26,6 @@ def predict():
                                result = 'The patient is likely to have heart disease!')
 
 if __name__ == '__main__':
-#Run the application
+    
     app.run(debug=True)
     
